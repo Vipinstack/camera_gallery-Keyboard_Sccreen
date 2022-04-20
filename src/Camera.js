358f190b0d601cfdd33f0ba6a0849a1a863eefd3
shailu121 <shailuakerstech8@gmail.com>
2022-04-20 12:23:33 +0530

    qr code update

--- src/Camera.js
+++ src/Camera.js
@@ -5,30 +5,35 @@ import {
   Image,
   TouchableOpacity,
   Platform,
+  Button,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Sharing from "expo-sharing";
 import * as MediaLibrary from "expo-media-library";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const Camera = () => {
   const [image, setImage] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
   let defaultImg = "https://i.postimg.cc/yxY28q4R/Ellipse-1.png";
 
-  //   console.log(image.localUri);
+
+
 
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
-
     if (permissionResult.granted === false) {
       alert("Permission to access camera roll is required!");
       return;
     }
-
+    
     let pickerResult = await ImagePicker.launchCameraAsync();
-    setImage({ localUri: pickerResult.uri });
-    const asset = await MediaLibrary.createAssetAsync(pickerResult.uri);
+     setImage({ localUri: pickerResult.uri })
+      const asset = await MediaLibrary.createAssetAsync(pickerResult.uri);
   };
 
   let openImageCameraPickerAsync = async () => {
@@ -49,15 +54,66 @@ const Camera = () => {
       alert(`Uh oh, sharing isn't available on your platform`);
       return;
     }
-
     await Sharing.shareAsync(image.localUri);
   };
 
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  };
+
+  // Request Camera Permission
+  useEffect(() => {
+    setTimeout(()=>{
+      askForCameraPermission();
+    },1000)
+    // setImage('');
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400,}}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+      <Text style={styles.logoQr}> Qr Code </Text>
+
       <Image
         source={{
           uri: `${image === null ? defaultImg : image.localUri}`,
+          // uri: `${image.localUri}`,
         }}
         style={styles.img}
       />
@@ -100,6 +156,18 @@ const Camera = () => {
         >
           <Entypo name="share" size={24} color="black" />
         </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderRadius: 5,
+            marginTop: 10,
+          }}
+        >
+          
+      {scanned && (
+        <MaterialCommunityIcons name="qrcode-scan" size={24} color="black" onPress={() => setScanned(false)} />    
+      )}
+
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -115,9 +183,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fcfcfc",
   },
   img: {
-    width: 300,
-    height: 300,
-    borderRadius: 20,
+    width: 200,
+    height: 200,
+    borderRadius: 150,
     elevation: 12,
+  },
+  maintext: {
+    position:'relative',
+    top:400,
+    fontSize: 16,
+    margin: 20,
+    color:'black'
+  },
+  logoQr:{
+    color:'white',
+    backgroundColor:'#303030',
+    padding:12,
+    borderRadius:6,
+    fontWeight:'bold',
+    fontSize:14,
+    position:'absolute',
+    top:120
+  },
+  barcodebox: {
+    position: "absolute",
+    top: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: 'tomato'
   },
 });
